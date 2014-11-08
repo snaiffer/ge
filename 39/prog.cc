a0508fa73c21f3b082730cb5c79a442a4290b452
Popov Dmitry <popov@informatics.com>
2014-11-08 17:06:33 +0400

    Число вхождений N в массив

---
+++ 39/prog.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+void outputArray(unsigned short* array, unsigned short& size) {
+    cout << " array = [ ";
+    for(unsigned short i = 0; i < size; i++)
+    cout << array[i] << ", "; 
+    cout << "]" << endl;
+}
+
+int main(int argc, char const *argv[]){
+    unsigned short size = 3;
+    cout << "Input size of array = "; cin >> size;
+    unsigned short* array = new unsigned short[size];
+    cout << "Input array:" << endl;
+    for(unsigned short i = 0; i < size; i++) {
+    cout << " array[" << i << "] = "; cin >> array[i];
+    }
+
+    cout << "\nInputed array:" << endl;
+    outputArray(array, size);
+	unsigned short n = 5; cout << "Input N = "; cin >> n;
+
+	unsigned short count = 0;
+	for(unsigned short i = 0; i < size; i++)
+		count += array[i] == n ? 1 : 0;
+
+	cout << "\nN occurrences in inputed array " << count << " times " << endl;
+
+    return 0;
+}
+
