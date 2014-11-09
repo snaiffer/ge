6c40d4be44fd72fa08696c2d7fad32aaf4b087ee
snaiffer <snaiffer@gmail.com>
2014-11-09 19:02:50 +0300

    28: unsigned short -> int

--- 28/prog.cc
+++ 28/prog.cc
@@ -1,38 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void outputArray(short* array, unsigned short& size) {
+void outputArray(int* array, int& size) {
   cout << " array = [ ";
-  for(unsigned short i = 0; i < size; i++)
+  for(int i = 0; i < size; i++)
     cout << array[i] << ", "; 
   cout << "]" << endl;
 }
 
 int main() {
-  unsigned short size = 3;
+  int size = 3;
   cout << "Input size of array = "; cin >> size;
-  short* array = new short[size];
+  int* array = new int[size];
 
   cout << "Input array:" << endl;
-  for(unsigned short i = 0; i < size; i++) {
+  for(int i = 0; i < size; i++) {
     cout << " array[" << i << "] = "; cin >> array[i];
   }
   cout << "\nInputed array:" << endl;
   outputArray(array, size);
+
   int maxElement = 0;
   int maxIndex = 0;
-  for(unsigned short i = 0; i < size; i++)
+  for(int i = 0; i < size; i++)
     if (array[i] > maxElement) {
       maxElement = array[i];
       maxIndex = i;
     }
-    // maxElement = array[i] > maxElement ? array[i] : maxElement;
+
   cout << "\nMax element: " << maxElement << endl;
-  cout << "\nMax element index: " << maxIndex + 1 << endl;
+  cout << "Max element index: " << maxIndex + 1 << endl;
   return 0;
 }
-
-/*
- * word type: 2 bytes, 0..65535
- * unsigned short in C++ is the same as word in Pascal
- */
